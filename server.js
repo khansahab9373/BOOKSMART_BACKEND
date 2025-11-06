0f25c0d5f2aa3bd3d1889df1c2e2280b7242e3fc
khansahab9373 <abdulrahmankhan1011@gmail.com>
2025-11-07 01:23:51 +0530

    update database link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import "./conn/conn.js";
-
+// import paymentRoutes from "./routes/payment.js";
 import User from "./routes/user.js";
 import Books from "./routes/book.js";
 import Favourite from "./routes/favourite.js";
@@ -25,6 +25,7 @@ app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
 app.use("/api/v1", Cart);
 app.use("/api/v1", Order);
+// app.use("/api/payment", paymentRoutes);
 
 // Creating port
 app.listen(process.env.PORT, () => {
